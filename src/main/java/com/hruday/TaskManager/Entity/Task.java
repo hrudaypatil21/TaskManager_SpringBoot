1604e726d5283a1983fb9eca7e59f8201ed5df11
hrudaypatil21 <hrudaypatil21@gmail.com>
2025-06-05 13:01:15 +0530

    Initial Commit

---
+++ src/main/java/com/hruday/TaskManager/Entity/Task.java
@@ -0,0 +1,54 @@
+package com.hruday.TaskManager.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "task")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+
+    private String description;
+
+    @Column(name = "due_date", nullable = false)
+    private LocalDate dueDate;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private Status status = Status.TODO;
+
+    private enum Status {
+        TODO("To Do"),
+        IN_PROGRESS("In Progress"),
+        DONE("Done");
+
+        private final String displayName;
+
+        Status(String displayName) {
+            this.displayName = displayName;
+        }
+
+        @Override
+        public String toString() {
+            return displayName;
+        }
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
---
+++ src/main/java/com/hruday/TaskManager/Entity/User.java
@@ -0,0 +1,27 @@
+package com.hruday.TaskManager.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "emp_id", nullable = false, unique = true)
+    private String empId;
+
+    @Column(name = "emp_email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
